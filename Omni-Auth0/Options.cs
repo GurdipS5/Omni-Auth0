2f13efaf1a9412c3a42908155a59090a563bbba6
GurdipS5 <gurdip.sira@gmail.com>
2024-04-21 02:09:18 +0100

    jjjh

---
+++ Omni-Auth0/Options.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniAuth0
+{
+    /// <summary>
+    ///
+    /// </summary>
+    internal class Options
+    {
+            /// <summary>
+            ///
+            /// </summary>
+            public string OmniUrl  { get; set; } = String.Empty;
+
+            /// <summary>
+            ///
+            /// </summary>
+            public string GoogleOauth2Id { get; set; } = String.Empty;
+    }
+}
---
+++ Omni-Auth0/Program.cs
@@ -0,0 +1,58 @@
+﻿using Pulumi;
+using Pulumi.Auth0;
+using Pulumi.Auth0.Inputs;
+using System.Collections.Generic;
+
+return await Deployment.RunAsync(() =>
+{
+
+    // Create an Auth0 Connection to enable Google social login.
+    var googleConnection = new Connection("google-oauth2-connection", new ConnectionArgs
+    {
+        Name = "google-oauth2",         // Unique name for the connection for referencing in your application
+        Strategy = "google-oauth2", // Specify the strategy as "google-oauth2" for Google social login
+
+        // Configuration options specific to the Google OAuth2 strategy
+        Options = new ConnectionOptionsArgs
+        {
+            ClientId = "your-google-client-id",
+            ClientSecret = "your-google-client-secret",
+
+            // The permissions that your application will request from Google
+            Scopes = { "email", "profile" },
+
+        },
+
+        // Indicates whether this connection can be used with Auth0 Development keys
+        IsDomainConnection = false
+    });
+
+    // create Auth0 client
+    var auth0Client = new Client("client", new ClientArgs
+    {
+        AllowedLogoutUrls =
+        {
+            "https://example.com/logout",
+        },
+        AllowedOrigins =
+        {
+            "https://example.com",
+        },
+        AppType = "regular_web",
+        Callbacks =
+        {
+            "https://example.com/auth/callback",
+        },
+        JwtConfiguration = new ClientJwtConfigurationArgs
+        {
+            Alg = "RS256",
+        },
+    });
+
+    // Export Client ID and Secret
+    return new Dictionary<string, object?>
+    {
+        ["clientID"] = auth0Client.ClientId,
+        ["clientSecret"] = auth0Client.ClientSecret
+    };
+});
